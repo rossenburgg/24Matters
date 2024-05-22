488d9ab4f708942c2a7fb7c6abe0348dafb0e6d7
Defc0n <90521128+rossenburgg@users.noreply.github.com>
2024-05-23 07:20:45 +0800

    polished 2fa

--- 24Matters/routes/authRoutes.js
+++ 24Matters/routes/authRoutes.js
@@ -61,27 +61,31 @@ router.post('/auth/login', async (req, res) => {
 });
 
 router.get('/auth/setup2fa', async (req, res) => {
-  if (!req.session.userId) {
+  if (!req.session.userId && !req.session.tempUserId) {
     return res.redirect('/auth/login');
   }
   try {
-    const user = await User.findById(req.session.userId);
-    const secret = speakeasy.generateSecret({ length: 20 });
-    req.session.tempSecret = secret.base32; // Store the secret temporarily in the session
-    const otpauthUrl = speakeasy.otpauthURL({
-      secret: secret.ascii,
-      label: encodeURIComponent('24Matters:' + user.username),
-      issuer: '24Matters',
-      encoding: 'ascii'
-    });
-    qrcode.toDataURL(otpauthUrl, (err, dataUrl) => {
-      if (err) {
-        console.error('Error generating QR code:', err);
-        console.error(err.stack);
-        throw err;
-      }
-      res.render('setup2fa', { qrCodeUrl: dataUrl });
-    });
+    const user = await User.findById(req.session.userId || req.session.tempUserId);
+    if (user.twoFactorSecret) {
+      res.render('setup2fa', { user, qrCodeUrl: null, twoFactorEnabled: true });
+    } else {
+      const secret = speakeasy.generateSecret({ length: 20 });
+      req.session.tempSecret = secret.base32; // Store the secret temporarily in the session
+      const otpauthUrl = speakeasy.otpauthURL({
+        secret: secret.ascii,
+        label: encodeURIComponent('24Matters:' + user.username),
+        issuer: '24Matters',
+        encoding: 'ascii'
+      });
+      qrcode.toDataURL(otpauthUrl, (err, dataUrl) => {
+        if (err) {
+          console.error('Error generating QR code:', err);
+          console.error(err.stack);
+          throw err;
+        }
+        res.render('setup2fa', { user, qrCodeUrl: dataUrl, twoFactorEnabled: false });
+      });
+    }
   } catch (error) {
     console.error('Error setting up 2FA:', error);
     console.error(error.stack);
@@ -153,6 +157,39 @@ router.post('/auth/2fa/verify', async (req, res) => {
     console.error('2FA verification error:', error);
     console.error(error.stack);
     res.status(500).send('An error occurred during 2FA verification.');
+  }
+});
+
+router.post('/auth/disable2fa', async (req, res) => {
+  if (!req.session.userId) {
+    console.error('Unauthorized attempt to disable 2FA without being logged in.');
+    return res.status(401).send('Unauthorized access.');
+  }
+  try {
+    const { disableToken } = req.body;
+    const user = await User.findById(req.session.userId);
+    console.log(`Attempting to disable 2FA for user: ${user.username} with token: ${disableToken} at time: ${new Date().toISOString()}`);
+    const verified = speakeasy.totp.verify({
+      secret: user.twoFactorSecret,
+      encoding: 'base32',
+      token: disableToken,
+      window: 1
+    });
+    console.log(`2FA token verification result: ${verified}`);
+
+    if (verified) {
+      user.twoFactorSecret = undefined;
+      await user.save();
+      console.log(`2FA disabled for user: ${user.username}`);
+      res.redirect('/auth/setup2fa');
+    } else {
+      console.error('Failed to disable 2FA: Invalid token');
+      res.status(401).send('Invalid 2FA token');
+    }
+  } catch (error) {
+    console.error('Error disabling 2FA:', error);
+    console.error(error.stack);
+    res.status(500).send('An error occurred while disabling 2FA.');
   }
 });
 
