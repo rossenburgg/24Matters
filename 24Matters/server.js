6878ae2bcabdfb820af5a88c56cdf39b3c80f6a9
Defc0n <90521128+rossenburgg@users.noreply.github.com>
2024-06-05 23:15:39 +0800

    updated server

--- 24Matters/server.js
+++ 24Matters/server.js
@@ -50,10 +50,12 @@ app.set("view engine", "ejs");
 // Serve static files
 app.use(express.static("public"));
 
-
 // Database connection
 mongoose
-  .connect(process.env.DATABASE_URL)
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log("Database connected successfully");
   })
@@ -108,7 +110,6 @@ io.on('connection', (socket) => {
     console.log('User disconnected');
   });
 
-
   socket.on('register', async (userId) => {
     if (!userId) {
       console.error("Attempt to register socket without userId");
@@ -124,7 +125,6 @@ io.on('connection', (socket) => {
       .catch(error => {
         console.error(`Error fetching initial unread notifications for user ${userId}: ${error}`);
       });
-
 
     try {
       const unreadCount = await Notification.countDocuments({ userId: new ObjectId(userId), read: false });
